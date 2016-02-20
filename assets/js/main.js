bb56e54a21e5717165a815db728763ebf34663c6
Julien Rousset <julien.rousset01@gmail.com>
2016-02-20 20:59:36 +0100

    SETUP :: Install VueJS / jQuery

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    new Vue({
+        el: '#app',
+        data: {
+            message:'Hello Vue.js!'
+        },
+        methods: {
+            reverseMessage : function(){
+                this.message = this.message.split('').reverse().join('');
+            }
+        }
+    })
+});
